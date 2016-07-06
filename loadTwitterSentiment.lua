0b6fc2466c76477537c5130bdb7bce13260de2c8
Sagar M. Waghmare <SW@discoverelement.com>
2016-07-06 13:55:49 -0400

    Completed parsing and generating vocabulary for Twitter data.

--- loadTwitterSentiment.lua
+++ loadTwitterSentiment.lua
@@ -21,16 +21,15 @@ local dl = require 'dataload._env'
    Recommended to use Torch with Lua installation
    (please refer to https://github.com/torch/distro for installation steps).
 --]]
-function dl.loadTwitterSentiment(datapath, validRatio, scale, srcUrl,
-                                                         showProgress)
+function dl.loadTwitterSentiment(datapath, validRatio, srcUrl, showProgress)
    -- 1. arguments and defaults
    
    -- path to directory containing Twitter dataset on disk
    datapath = datapath or paths.concat(dl.DATA_PATH, 'twitter')
+
    -- proportion of training set to use for cross-validation.
    validRatio = validRatio or 1/8
-   -- scales the values between this range
-   scale = scale == nil and {0,1} or scale
+
    -- URL from which to download dataset if not found on disk.
    srcUrl = srcUrl or 'http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip'
    -- debug
@@ -67,46 +66,16 @@ function dl.loadTwitterSentiment(datapath, validRatio, scale, srcUrl,
    if showProgress then print("Load & processing training data.") end
    local trainTweetInfos, trainTweets, allTrainWords, maxTweetLen = 
                                           dl.processTwitterCSV(trainDataFile)
-   print(maxTweetLen)
-   local vocab, vocab, wordFreq = dl.buildVocab(allTrainWords)
+   local vocab, ivocab, wordFreq = dl.buildVocab(allTrainWords)
    allTrainWords = nil
    collectgarbage()
 
    -- Load Test File
    if showProgress then print("Load & processing testing data.") end
-   local testTweetInfos, testTweets, allTrainWords, maxTweetLen = 
+   local testTweetInfos, testTweets, allTestWords, maxTweetLen = 
                          dl.processTwitterCSV(testDataFile, maxTweetLen, false)
-   print(maxTweetLen)
-   return train, valid, test
-end
-
-function dl.loadTwitterCSV(filename, sep, contentsep, showprogress)
-   local sep = sep or '","'
-   local contentsep = contentsep or ' '
-   local showprogress = showprogress or false
-   if showprogress then
-      print('loading ' .. filename, sep, contentsep)
-   end
-   local nlines = dl.getNumberOfLines(filename)
-   local filelines = io.open(filename):lines()
-   local fieldstable, contenttable = {}, {}
-   local tablesize=0
-   for line in filelines do
-      local vs = dl.splitString(line, sep)
-      vs[1] = vs[1]:sub(2,#vs[1])
-      vs[6] = vs[6]:sub(1,#vs[6]-1)
-      vs[6] = dl.splitString(vs[6], contentsep)
-      table.insert(fieldstable, vs)
-      table.insert(contenttable, vs[6])
-      tablesize = tablesize + 1
-      if showprogress and math.fmod(tablesize, 100)==0 then
-         xlua.progress(tablesize, nlines)
-      end
-   end
-   if showprogress then
-      print('# items loaded ' .. tablesize)
-   end
-   return fieldstable, contenttable
+   print(allTestWords)
+   return vocab, ivocab, wordFreq
 end
 
 function dl.processTwitterCSV(filename, maxTweetLen, returnAllWords)
@@ -146,10 +115,4 @@ function dl.processTwitterCSV(filename, maxTweetLen, returnAllWords)
       tweets[tweetId] = tweet
    end
    return tweetsInfo, tweets, allWords, maxTweetLen
-end
-
-function dl.buildContentVocab(contenttable)
-   local tokens = contenttable --stringx.split(text)
-   vocab, ivocab, wordfreq = dl.buildVocab(tokens)
-   return vocab, ivocab, wordfreq
 end
